fc13b0bb5fa107b7a0931e7d496816b5e243f3f6
helena-donaldson <hmdonaldson5@gmail.com>
2025-06-07 15:33:19 -0400

    Last alterations

--- assignment1CodePath/src/components/CardGrid.jsx
+++ assignment1CodePath/src/components/CardGrid.jsx
@@ -10,7 +10,7 @@ const CardGrid = () => {
     <Card webLink='https://women.acm.org/' imgLink='https://th.bing.com/th/id/OIP.rm4cRHVOvUs1GTH7tavDOgHaEo?r=0&rs=1&pid=ImgDetMain' imgDescription='ACM-W Logo' description='ACM-W'/>
     <Card webLink='https://wie.ieee.org/' imgLink='https://ieee.ae/wp-content/uploads/sites/158/2021/07/WIE_purple_pms526.png' imgDescription='IEEE Women in Engineering logo' description='IEEE Women in Engineering'/>
     <Card webLink='https://rewritingthecode.org/'imgLink='https://alfacharlie.co/wp-content/uploads/2022/10/Rewriting-the-Code-Logo-Design.jpg' imgDescription='Rewriting the Code Logo' description='Rewriting the Code'/>
-    <Card webLink='https://rewritingthecode.org/' imgLink='https://mma.prnewswire.com/media/1800392/codepath_1x1_solid_dark_Logo.jpg?p=publish' imgDescription='CodePath Logo' description='CodePath'/>
+    <Card webLink='https://www.codepath.org/courses' imgLink='https://mma.prnewswire.com/media/1800392/codepath_1x1_solid_dark_Logo.jpg?p=publish' imgDescription='CodePath Logo' description='CodePath'/>
     <Card webLink='https://girldevelopit.com/' imgLink='https://images.crunchbase.com/image/upload/c_pad,h_256,w_256,f_auto,q_auto:eco,dpr_1/v1416993852/mdqpcqunqezk1k3wnet1.png' imgDescription='Girls Develop It Logo' description='Girls Develop It'/>
     <Card webLink='https://ncwit.org/' imgLink='https://vepimg.b8cdn.com/uploads/vjfnew/9076/uploads/vjf/content/misc/1678194885logo.png' imgDescription='NCWIT Logo' description='National Center for Women in Technology'/>
     <Card webLink='https://ai-4-all.org/' imgLink='https://www.mdc.edu/entec/img/aiforall-logo.jpg' imgDescription='AI4ALL Logo' description='AI4ALL'/>
